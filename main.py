433cf9089cdb32ae3b93df311b909035301ea6e1
Your Name <you@example.com>
2026-08-11 16:55:03 -0600

    Agregar bandera --test
    Se elimina el hardcoding de los sensores de prueba

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import argparse
+
 from app.application import ApplicationController
 from gui.interface import ControlInterface
 from hardware.esp32 import ESP32Sensor
@@ -8,30 +10,44 @@ from hardware.simulated_motor import SimulatedMotor
 from hardware.simulated_sensor import SimulatedESP32Sensor
 
 
-SIMULATION = True
+def parse_args() -> argparse.Namespace:
+
+    parser = argparse.ArgumentParser(
+        description="Refractómetro"
+    )
+
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help=(
+            "Usar hardware simulado en vez de conectar "
+            "al ESP32 y al motor Zaber reales."
+        ),
+    )
+
+    return parser.parse_args()
+
 
 def main() -> None:
     """
     Punto de entrada de la aplicación.
     """
 
+    args = parse_args()
+
     # Crear hardware
-    if SIMULATION:
+    if args.test:
         motor = SimulatedMotor()
         sensor = SimulatedESP32Sensor(motor)
-
     else:
         motor = ZaberMotor()
         sensor = ESP32Sensor()
-
 
     # Inyectar hardware al controlador
     controller = ApplicationController(
         motor=motor,
         sensor=sensor,
     )
-
-
 
     # Crear interfaz gráfica
     interface = ControlInterface(controller=controller,)
@@ -40,7 +56,6 @@ def main() -> None:
     interface.construir()
     interface.ejecutar()
     interface.cerrar()
-
 
 
 if __name__ == "__main__":
